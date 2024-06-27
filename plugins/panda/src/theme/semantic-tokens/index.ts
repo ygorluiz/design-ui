5159e44bf37fb6d7a673dcd12557f39a86e79c71
Ygor Luiz <ygorluiz@me.com>
2024-06-27 05:02:02 -0300

    feat: add new semantics tokens

--- plugins/panda/src/theme/semantic-tokens/index.ts
+++ plugins/panda/src/theme/semantic-tokens/index.ts
@@ -57,6 +57,160 @@ export const createSemanticTokens = (options: PresetOptions) => {
         disabled: { value: '{colors.gray.5}' },
         outline: { value: '{colors.gray.a9}' },
       },
+
+      /*
+      custom colors
+      */
+      accents: {
+        value: {
+          base: '{colors.bluePalette.9}',
+          _dark: '{colors.bluePalette.8}',
+        },
+      },
+      background: {
+        value: {
+          base: '{colors.grayPalette.1}',
+          _dark: '{colors.grayPalette.1}',
+        },
+      },
+      header: {
+        value: {
+          base: 'oklch(from {colors.grayPalette.1} l c h / 40%)',
+          _dark: 'oklch(from {colors.grayPalette.1} l c h / 40%)',
+        },
+      },
+      emphasis: {
+        value: {
+          base: 'oklch(from {colors.bluePalette.9} l c h / 5%)',
+          _dark: 'oklch(from {colors.bluePalette.8} l c h / 7%)',
+        },
+      },
+      foreground: {
+        value: {
+          base: 'oklch( from {colors.grayPalette.2} l c h / 100%)',
+          _dark: '{colors.grayPalette.3}',
+        },
+      },
+      danger: {
+        value: {
+          base: '{colors.redPalette.9}',
+          _dark: '{colors.redPalette.10}',
+        },
+      },
+      dangerEmphasis: {
+        value: {
+          base: 'oklch(from {colors.redPalette.7} l c h / 10%)',
+          _dark: 'oklch(from {colors.redPalette.10} l c h / 10%)',
+        },
+      },
+      warning: {
+        value: {
+          base: '{colors.orangePalette.11}',
+          _dark: '{colors.orangePalette.10}',
+        },
+      },
+      warningEmphasis: {
+        value: {
+          base: 'oklch(from {colors.orangePalette.11} l c h / 7%)',
+          _dark: 'oklch(from {colors.orangePalette.10} l c h / 10%)',
+        },
+      },
+      success: {
+        value: {
+          base: '{colors.greenPalette.11}',
+          _dark: '{colors.greenPalette.9}',
+        },
+      },
+      successEmphasis: {
+        value: {
+          base: 'oklch(from {colors.greenPalette.9} l c h / 10%)',
+          _dark: 'oklch(from {colors.greenPalette.9} l c h / 10%)',
+        },
+      },
+      textPrimary: {
+        value: {
+          base: '{colors.grayPalette.12}',
+          _dark: '{colors.grayPalette.4}',
+        },
+      },
+      textSecondary: {
+        value: {
+          base: 'var(--gray1100)',
+          _dark: 'var(--gray1100)',
+        },
+      },
+      textTertiary: {
+        value: {
+          base: '{colors.grayPalette.10}',
+          _dark: '{colors.grayPalette.10}',
+        },
+      },
+      borderColor: {
+        value: {
+          base: '{colors.grayPalette.4}',
+          _dark: '{colors.grayPalette.4}',
+        },
+      },
+      cardBackgroundColor: {
+        value: {
+          base: '{colors.grayPalette.2}',
+          _dark: '{colors.grayPalette.3}',
+        },
+      },
+
+      input: {
+        active: {
+          value: {
+            base: '{colors.bluePalette.9}',
+            _dark: '{colors.bluePalette.8}',
+          },
+        },
+        background: { value: { base: '{colors.grayPalette.0}', _dark: '{colors.grayPalette.0}' } },
+        disabled: {
+          value: { base: '{colors.grayPalette.3}', _dark: '{colors.grayPalette.7}' },
+        },
+        border: {
+          value: { base: '{colors.grayPalette.5}', _dark: '{colors.grayPalette.6}' },
+        },
+        focus: { value: { base: '{colors.bluePalette.8}', _dark: '{colors.bluePalette.9}' } },
+      },
+
+      shadowColor: {
+        value: {
+          base: 'var(--local-shadow-color, 223deg 5% 80%)',
+          _dark: 'var(--local-shadow-color, 223deg 5% 1%)',
+        },
+      },
+      snippet: {
+        value: {
+          base: '{colors.grayPalette.2}',
+          _dark: '{colors.grayPalette.3}',
+        },
+      },
+      token: {
+        comment: {
+          value: {
+            base: '{colors.grayPalette.10}',
+            _dark: '{colors.grayPalette.10}',
+          },
+        },
+        selector: { value: { base: '{colors.bluePalette.9}', _dark: '{colors.bluePalette.6}' } },
+        symbol: {
+          value: { base: '{colors.bluePalette.11}', _dark: '{colors.bluePalette.8}' },
+        },
+        operator: {
+          value: { base: '{colors.grayPalette.10}', _dark: '{colors.orangePalette.12}' },
+        },
+        keyword: {
+          value: { base: '{colors.bluePalette.10}', _dark: '{{colors.bluePalette.7}' },
+        },
+        function: {
+          value: { base: '{colors.pinkPalette.10}', _dark: '{colors.pinkPalette.4}' },
+        },
+        punctuation: {
+          value: { base: '{colors.bluePalette.9}', _dark: '{colors.bluePalette.6}' },
+        },
+      },
     },
     shadows,
     radii: createRadiiTokens(borderRadius),
